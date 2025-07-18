2aafffd56e326c6114be3b0bd4dc3d336bcd37b7
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-07-18 17:07:39 -0500

    pathing fix in gru embedder

--- slurm_jobs/world_2_neighbor_analysis/cancel_world2_pipeline.sh
+++ slurm_jobs/world_2_neighbor_analysis/cancel_world2_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo '--- 🛑 Sending stop signal to all World 2 pipeline jobs! 🛑 ---'
-scancel 1323904 # W2_Pipeline_gru_V-6_S-50
-scancel 1323905 # W2_Pipeline_transformer_V-6_S-50
+scancel 1323906 # W2_Pipeline_gru_V-6_S-50
+scancel 1323907 # W2_Pipeline_transformer_V-6_S-50
