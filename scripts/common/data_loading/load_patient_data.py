cc94c00b054c1dc6e0843bcbd7365daa6f30c1a4
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-12-05 16:03:36 -0600

    using katie work

--- scripts/common/data_loading/load_patient_data.py
+++ scripts/common/data_loading/load_patient_data.py
@@ -4,13 +4,23 @@ from pathlib import Path
 import multiprocessing
 import random
 from dotenv import load_dotenv
+load_dotenv()
 import json
+import pandas as pd
 
 from common.data_loading.fit_to_anchor import slice_and_convert_time, find_anchor_date
 
-PROJECT_ROOT = Path(__file__).resolve().parents[3]
+COHORT_PATH = Path(os.environ['COHORT_PATH'])
+SEED = int(os.environ['SEED'])
+NUM_PATIENTS = int(os.environ['NUM_PATIENTS'])
+RAW_JSON_PATH = Path(os.environ['PATIENT_JSON_DIR'])
+SLICED_JSON_PATH = Path(os.environ['SLICED_PATIENT_JSON_DIR'])
 
-load_dotenv()
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+ARTIFACTS_PATH = PROJECT_ROOT / "artifacts"
+ARTIFACTS_PATH.mkdir(parents=True, exist_ok=True)
+SAMPLED_IDS_PATH = ARTIFACTS_PATH / f"{NUM_PATIENTS}_patients/sampled_patient_ids.txt"
+SAMPLED_IDS_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 def _load_one_patient(paths: tuple[Path,Path]) -> Dict:
     sliced_patient_json_file_path, original_patient_json_file_path = paths[0], paths[1]
@@ -26,7 +36,7 @@ def _load_one_patient(paths: tuple[Path,Path]) -> Dict:
             patient_json = json.load(f)
             anchor_date = find_anchor_date(patient_json)
             if not anchor_date:
-                raise ValueError(f"Patient at path {original_patient_json_file_path} does not have relevant diagnosis.")
+                raise ValueError(f"Patient at path {original_patient_json_file_path} does not have relevant medication.")
             sliced_json = slice_and_convert_time(patient_json, anchor_date, int(os.environ['YEARS_BACK']))
             
             sliced_patient_json_file_path.parent.mkdir(parents=True, exist_ok=True)
@@ -34,44 +44,45 @@ def _load_one_patient(paths: tuple[Path,Path]) -> Dict:
                 # Store the sliced json now that we have created it
                 json.dump(sliced_json, f, indent=4)
             return sliced_json
-        
-def _see_if_anchor(json_path: Path) -> Optional[str]:
-    with open(json_path, 'r') as f:
-        patient_dict = json.load(f)
-        if find_anchor_date(patient_dict):
-            return patient_dict['patient_id']
-        return None
-
-def load_patient_data() -> Iterator[Dict]:
-    seed = int(os.environ['SEED'])
-    num_patients = int(os.environ['NUM_PATIENTS'])
-    raw_json_path = Path(os.environ['PATIENT_JSON_DIR'])
-    sliced_json_path = Path(os.environ['SLICED_PATIENT_JSON_DIR'])
-    artifacts_path = PROJECT_ROOT / "artifacts"
-    artifacts_path.mkdir(parents=True, exist_ok=True)
-    sampled_ids_path = artifacts_path / f"{num_patients}_patients/sampled_patient_ids.txt"
-    sampled_ids_path.parent.mkdir(parents=True, exist_ok=True)
-    all_eligible_path = artifacts_path / "all_patient_ids_with_diagnosis.txt"
     
-    if not sampled_ids_path.exists():
-        # We must sample and from all potential patients of interest
-        relevant_ids = []
-        if not all_eligible_path.exists():
-            # We need to know who our valid patients with the relevant diagnosis we care about are
-            all_raw_json = raw_json_path.glob("*.json")
-            with multiprocessing.Pool(processes=int(os.environ['NUM_WORKERS_NON_LLM_TASK'])) as thread_pool:
-                for id in thread_pool.imap_unordered(_see_if_anchor, all_raw_json):
-                    if id:
-                        relevant_ids.append(id)
-        rnd = random.Random(seed)
-        random_valid_ids = rnd.sample(relevant_ids, num_patients)
-        with open(sampled_ids_path, 'w') as f:
-            f.write("\n".join(random_valid_ids))
+def _check_anchor_for_id(patient_id: str) -> Optional[str]:
+    # Worker function
+    if _see_if_anchor(RAW_JSON_PATH / f"patient_{patient_id}.json"):
+        return patient_id
+    return None
         
-    with open(sampled_ids_path, 'r') as f:
+def _see_if_anchor(json_path: Path) -> bool:
+    # Helper function FOR the worker function
+    try:
+        with open(json_path, 'r') as f:
+            patient_dict = json.load(f)
+            if find_anchor_date(patient_dict):
+                return True
+            return False
+    except Exception:
+        return False
+    
+def load_patient_data() -> Iterator[Dict]:
+    
+    if not SAMPLED_IDS_PATH.exists():
+        cohort_df = pd.read_csv(COHORT_PATH)
+        patient_ids_with_mdd = cohort_df['PatientEpicId_SH'].tolist()
+        patient_ids_with_anchor = []
+        with multiprocessing.Pool(processes=int(os.environ['NUM_WORKERS_NON_LLM_TASK'])) as pool:
+            for id in pool.imap_unordered(_check_anchor_for_id, patient_ids_with_mdd):
+                if id:
+                    patient_ids_with_anchor.append(id)
+        
+        rnd = random.Random(SEED)
+        sampled_patients = rnd.sample(patient_ids_with_anchor, k=min(NUM_PATIENTS, len(patient_ids_with_anchor)))
+        os.makedirs(SAMPLED_IDS_PATH.parent, exist_ok=True)
+        with open(SAMPLED_IDS_PATH, 'w') as f:
+            f.write("\n".join(sampled_patients))
+        
+    with open(SAMPLED_IDS_PATH, 'r') as f:
         # Ignore empty lines
         cohort_ids = [line.strip() for line in f if line.strip()]
         # Create list of tuples - the first element is the patient's sliced json path, and the second is the patient's original json path
-        sliced_and_unsliced_paths = [(sliced_json_path / f'patient_{id}.json', raw_json_path / f'patient_{id}.json') for id in cohort_ids]
+        sliced_and_unsliced_paths = [(SLICED_JSON_PATH / f'patient_{id}.json', RAW_JSON_PATH / f'patient_{id}.json') for id in cohort_ids]
         with multiprocessing.Pool(processes=int(os.environ['NUM_WORKERS_NON_LLM_TASK'])) as thread_pool:
             yield from thread_pool.imap_unordered(_load_one_patient, sliced_and_unsliced_paths)
