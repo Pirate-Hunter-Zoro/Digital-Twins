6f864d3730338399da577fd56c0c9913e8cd8bde
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-01-07 16:16:11 -0600

    trd from katie

---
+++ scripts/digital_twins/predictions/trd_predictor.py
@@ -0,0 +1,9 @@
+import numpy as np
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from scripts.digital_twins.neighbors.retriever import Retriever
+from scripts.digital_twins.neighbors.scorer import Scorer
+
