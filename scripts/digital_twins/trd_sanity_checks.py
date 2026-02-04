dda279bfa626f4c4de1bf4033fdb9a620791c03c
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-02-03 20:03:36 -0600

    started trd sanity checks

--- scripts/digital_twins/trd_sanity_checks.py
+++ scripts/digital_twins/trd_sanity_checks.py
@@ -1,16 +1,26 @@
 import sqlite3
 import os
-from pathlib import Path
+import random
 
 from scripts.digital_twins.predictions.trd_predictor import TRDPredictor
+from scripts.digital_twins.neighbors.retriever import Retriever
 
 from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    # TODO - load all of vectors
-    connection = sqlite3.connect(Path(os.environ['VECTORS_DIR']) / 'vectors.db')
-    
-    predictor = TRDPredictor()
-    
-    pass
+    retriever = Retriever()
+    pair_ids = set()
+    all_hash_ids = retriever.ids
+    all_vectors = retriever.vectors
+    pairs = []
+    for _ in range(int(os.environ['NUM_PAIRS_SANITY_CHECK'])):
+        pair_indices = random.sample(range(len(all_hash_ids)), 2)
+        pair_indices = (pair_indices[0], pair_indices[1]) if pair_indices[0] < pair_indices[1] else (pair_indices[1], pair_indices[0])
+        while pair_indices in pair_ids:
+            pair_indices = random.sample(range(len(all_hash_ids)), 2)
+            pair_indices = (pair_indices[0], pair_indices[1]) if pair_indices[0] < pair_indices[1] else (pair_indices[1], pair_indices[0])  
+        pair_ids.add(pair_indices)
+        first_idx = pair_indices[0]
+        second_idx = pair_indices[1]
+        pairs.append(((all_hash_ids[first_idx], all_vectors[first_idx]), (all_hash_ids[second_idx], all_vectors[second_idx])))
