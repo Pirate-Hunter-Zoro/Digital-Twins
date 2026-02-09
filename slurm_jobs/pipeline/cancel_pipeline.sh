fe53635bfd259329eec92f6fe1c0c7a0545f782f
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-02-09 17:11:27 -0600

    json memory fix

--- slurm_jobs/pipeline/cancel_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Killing prediction pipeline...'
-scancel 1682914 # Embedding Job
-scancel 1682915 # vLLM Server
-scancel 1682916 # Prediction Pipeline
-scancel 1682917 # Analysis Pipeline
-scancel 1682913 # Orchestrator (Self)
