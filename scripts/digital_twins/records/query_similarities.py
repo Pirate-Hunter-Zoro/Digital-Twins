8d4a41de1a0166cf17055fe91f5a68fdae2b966f
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-04-23 19:43:47 -0500

    embedding query fix

--- scripts/digital_twins/records/query_similarities.py
+++ scripts/digital_twins/records/query_similarities.py
@@ -7,34 +7,34 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
-VECTORS_DB = Path(os.environ['EMBEDDINGS_DIR']) / "embeddings.db"
+EMBEDDINGS_DB = Path(os.environ['EMBEDDINGS_DIR']) / "embeddings.db"
 JUDGEMENTS_DB = Path(os.environ['JUDGEMENTS_DIR']) / "judgements.db"
 OUTPUT_DIR = Path("test_data/judgements")
 SAMPLE_SIZE = 10
 
 def main():
-    vector_connection = sqlite3.connect(VECTORS_DB)
-    vector_cursor = vector_connection.cursor()
-    judgement_connection = sqlite3.connect(JUDGEMENTS_DB)
-    judgement_cursor = judgement_connection.cursor()
+    embeddings_connection = sqlite3.connect(EMBEDDINGS_DB)
+    embeddings_cursor = embeddings_connection.cursor()
+    judgements_connection = sqlite3.connect(JUDGEMENTS_DB)
+    judgements_cursor = judgements_connection.cursor()
     
     # Grab some judgements
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-    judgement_cursor.execute("""
+    judgements_cursor.execute("""
 SELECT patient_id_a, patient_id_b, overall_score, full_response FROM llm_judgements                          
 """)
-    rows = judgement_cursor.fetchall()
+    rows = judgements_cursor.fetchall()
     for row in random.sample(rows, min(len(rows), SAMPLE_SIZE)):
         id_a, id_b, overall_score, full_response = row
         # Grab the narratives
-        vector_cursor.execute("""
-SELECT text, patient_id FROM vectors WHERE patient_id = ?
+        embeddings_cursor.execute("""
+SELECT text, patient_id FROM embeddings WHERE patient_id = ?
 """, (id_a,))
-        narrative_a, patient_id_a = vector_cursor.fetchone()
-        vector_cursor.execute("""
-SELECT text, patient_id FROM vectors WHERE patient_id = ?
+        narrative_a, patient_id_a = embeddings_cursor.fetchone()
+        embeddings_cursor.execute("""
+SELECT text, patient_id FROM embeddings WHERE patient_id = ?
 """, (id_b,))
-        narrative_b, patient_id_b = vector_cursor.fetchone()
+        narrative_b, patient_id_b = embeddings_cursor.fetchone()
         
         header = f"COMPARISION: {patient_id_a} vs {patient_id_b}"
         
@@ -52,8 +52,8 @@ SELECT text, patient_id FROM vectors WHERE patient_id = ?
         with open(txt_file, 'w') as f:
             f.write(contents)
             
-    vector_connection.close()
-    judgement_connection.close()
+    embeddings_connection.close()
+    judgements_connection.close()
             
 if __name__ == "__main__":
     main()
